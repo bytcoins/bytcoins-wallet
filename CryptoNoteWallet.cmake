d9d4a92e08d949982f5803ffb8ec9eabcee75709
Gonzalo Linares Amezcua <escreativa@escreativa.com>
2017-08-02 19:53:02 +0200

    Update CryptoNoteWallet.cmake

--- CryptoNoteWallet.cmake
+++ CryptoNoteWallet.cmake
@@ -1,4 +1,4 @@
 
-set(CN_PROJECT_NAME "")
-set(CN_CURRENCY_DISPLAY_NAME "")
-set(CN_CURRENCY_TICKER "")
+set(CN_PROJECT_NAME "bytcoins")
+set(CN_CURRENCY_DISPLAY_NAME "bytcoins")
+set(CN_CURRENCY_TICKER "bytcoins")
